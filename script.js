6d93dbe329f812a49105a5042711db3b0d9d6377
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-02-02 13:48:09 +0800

    updated data

--- script.js
+++ script.js
@@ -185,3 +185,13 @@ petshop.forEach((pet) => {
 
   document.body.appendChild(card);
 });
+
+// const user = {
+//   name: "John",
+//   age: 30,
+//   isAdmin: true,
+// };
+
+// for (let key in user) {
+//   console.log(key, user[key]);
+// }
